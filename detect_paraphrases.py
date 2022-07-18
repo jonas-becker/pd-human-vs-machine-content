713fe4b45453a21a9d2821e726640683253652c8
snakeeye98 <snakeeye98@web.de>
2022-07-18 13:08:31 +0200

    polishing

--- detect_paraphrases.py
+++ detect_paraphrases.py
@@ -7,10 +7,13 @@ from thefuzz import fuzz
 from setup import *
 import xml.etree.ElementTree as ET
 from sklearn.utils import shuffle
+from sklearn.feature_extraction.text import TfidfVectorizer
 import re
 import sys
 from gensim.utils import simple_preprocess
+from sklearn.metrics.pairwise import cosine_similarity
 import gensim.downloader as api
+from sentence_transformers import SentenceTransformer
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
 from gensim.similarities import SparseTermSimilarityMatrix, WordEmbeddingSimilarityIndex, SoftCosineSimilarity, Similarity
@@ -23,6 +26,11 @@ def preprocess(doc):
     doc = sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', " url_token ", doc)
     return [token for token in simple_preprocess(doc, min_len=0, max_len=float("inf")) if token not in STOPWORDS]
 
+def ngrams(string, n=3):
+    string = re.sub(r'[,-./]|\sBD',r'', string)
+    ngrams = zip(*[string[i:] for i in range(n)])
+    return [''.join(ngram) for ngram in ngrams]
+
 def check_semantic(corpus, string_2, similarity_matrix, tfidf, dictionary):
     # returns the semantic similarity based on the corpus
     query = preprocess(string_2)
@@ -30,8 +38,9 @@ def check_semantic(corpus, string_2, similarity_matrix, tfidf, dictionary):
     index = SoftCosineSimilarity(tfidf[[dictionary.doc2bow(document) for document in corpus]], similarity_matrix)
     return index[query_tf]
 
-def semantic_sim(df):
-    corpus = [ preprocess(document) for document in list(df["text_1"]) ]
+def semantic_sim_glove(df):
+    print("Calculating semantic similarity with GLoVe.")
+    corpus = [ preprocess(document) for document in list(df[TEXT1]) ]
     # use a pre trained model: https://huggingface.co/fse/glove-wiki-gigaword-50 , https://nlp.stanford.edu/pubs/glove.pdf
     glove = api.load("glove-wiki-gigaword-50")
     similarity_index = WordEmbeddingSimilarityIndex(glove)
@@ -44,7 +53,7 @@ def semantic_sim(df):
     print("Processing texts...")
     semantic_results = []
     for i, row in tqdm(df.iterrows(), total=df.shape[0]):
-        sim = check_semantic(corpus, row["text_2"], similarity_matrix, tfidf, dictionary)
+        sim = check_semantic(corpus, row[TEXT2], similarity_matrix, tfidf, dictionary)
         try:
             semantic_results.append(sim[i])
         except Exception as e:
@@ -54,13 +63,88 @@ def semantic_sim(df):
             continue
     return semantic_results
 
+def semantic_sim_bert(df):
+    print("Calculating semantic similarity with BERT.")
+    corpus1 = list(df[TEXT1])
+    corpus2 = list(df[TEXT2])
+    # use bert to embed
+    model = SentenceTransformer('all-mpnet-base-v2')
+    print("Encoding text_1's...")
+    text1_embeddings = model.encode(corpus1)
+    print("Encoding text_2's...")
+    text2_embeddings = model.encode(corpus2)
+
+    print("Processing texts...")
+    semantic_results = []
+    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+        sim = cosine_similarity([text1_embeddings[i]], [text2_embeddings[i]])[0][0]
+        try:
+            semantic_results.append(sim)
+        except Exception as e:
+            semantic_results.append(float(sim.item()))
+            continue
+    return semantic_results
+
+def semantic_sim_t5(df):
+    print("Calculating semantic similarity with T5.")
+    corpus1 = list(df[TEXT1])
+    corpus2 = list(df[TEXT2])
+    # use bert to embed
+    model = SentenceTransformer('sentence-t5-base')
+    print("Encoding text_1's...")
+    text1_embeddings = model.encode(corpus1)
+    print("Encoding text_2's...")
+    text2_embeddings = model.encode(corpus2)
+
+    print("Processing texts...")
+    semantic_results = []
+    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+        sim = cosine_similarity([text1_embeddings[i]], [text2_embeddings[i]])[0][0]
+        try:
+            semantic_results.append(sim)
+        except Exception as e:
+            semantic_results.append(float(sim.item()))
+            continue
+    return semantic_results
+
+def ngram_sim(df):
+    print("Calculating similarity with N-Grams and their TF-IDF cosine similarity.")
+    corpus1 = list(df[TEXT1])
+    corpus2 = list(df[TEXT2])
+
+    print("Processing texts...")
+    vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
+    tf_idf_matrix = vectorizer.fit_transform(corpus1+corpus2)   # combine text1 and text2 to one corpus
+    
+    results = []
+    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+        sim = cosine_similarity(tf_idf_matrix[i], tf_idf_matrix[len(corpus1)+i])    #calculate sim between text1 and text2 pairwise
+        results.append(sim[0][0])
+    return results
+
 def fuzzy_sim(df):
     #Check for paraphrase with fuzzy based
     fuzzy_results = []
     print("Checking for paraphrases with the fuzzy-based method. Dataframe rows to process: " + str(len(df)))
-    for i, row in tqdm(df.iterrows()):
-        fuzzy_results.append(float(fuzz.ratio(row["text_1"], row["text_2"])/100))
+    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+        fuzzy_results.append(float(fuzz.ratio(row[TEXT1], row[TEXT2])/100))
     return fuzzy_results
+
+def tfidf_cosine_sim(df):
+    print("Calculating TF-IDF cosine similarities.")
+    corpus1 = list(df[TEXT1])
+    corpus2 = list(df[TEXT2])
+
+    print("Processing texts...")
+    vectorizer = TfidfVectorizer()
+    tf_idf_matrix = vectorizer.fit_transform(corpus1+corpus2)   # combine text1 and text2 to one corpus
+    
+    results = []
+    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+        sim = cosine_similarity(tf_idf_matrix[i], tf_idf_matrix[len(corpus1)+i])    #calculate sim between text1 and text2 pairwise
+        results.append(sim[0][0])
+    return results
+
 
 #################################################################################
 
@@ -73,29 +157,35 @@ for embedded_file in os.listdir(os.path.join(OUT_DIR, EMBEDDINGS_FOLDER)):
     dataset = df.iloc[0][DATASET]
 
     # Modify datasets to be balanced (paraphrase & non-paraphrase)
+    '''
     if len(df[df[PARAPHRASE] == False]) < 10:    # if there is no noteworthy amount of original pairs, add them from other datasets (e.g. machine-datasets)
         # get paraphrase types for all pair IDs (read from different files)
         paraphrase_types = {}
         for filler_dataset in FILLER_DATASETS:
             df_tmp = pd.read_json(os.path.join(OUT_DIR, EMBEDDINGS_FOLDER, filler_dataset+"_embedded.json"), orient= "index")
-            print(df_tmp.shape[0])
             df_tmp = df_tmp[df_tmp[PARAPHRASE] == False]
             print(f"Adding {df_tmp.shape[0]} original pairs from {filler_dataset} to {dataset} for balancing.")
             df = pd.concat([df, df_tmp], ignore_index = True)   #concat the lastly processed dataset to the combined dataset
+    '''
 
     # Truncate "to much" data for balancing
-    df = shuffle(df).reset_index(drop=True)
-    df = df.groupby(PARAPHRASE)
-    df = pd.DataFrame(df.apply(lambda x: x.sample(df.size().min()).reset_index(drop=True)))
+    if len(df[df[PARAPHRASE] == False]) < 10:   # only truncate datasets that are not paraphrase-pairs only
+        df = shuffle(df).reset_index(drop=True)
+        df = df.groupby(PARAPHRASE)
+        df = pd.DataFrame(df.apply(lambda x: x.sample(df.size().min()).reset_index(drop=True)))
     df = shuffle(df).reset_index(drop=True)
     print("Balanced dataset with the following paraphrased-statistics:")
     print(df[PARAPHRASE].value_counts())
  
     # Calculate the similarities with each method
+    df[TFIDF_COSINE] = tfidf_cosine_sim(df)
+    df[NGRAM] = ngram_sim(df)  # maybe not working for strings of different length
     df[FUZZY] = fuzzy_sim(df)
-    df[SEMANTIC] = semantic_sim(df)
+    df[SEM_BERT] = semantic_sim_bert(df)
+    df[SEM_T5] = semantic_sim_t5(df)
+    #df[SEM_GLOVE] = semantic_sim_glove(df)
 
     #Output data to json format
     df.to_json(os.path.join(OUT_DIR, DETECTION_FOLDER, embedded_file.split("_")[0]+"_result.json"), orient = "index", index = True, indent = 4)
 
-
+print("Done.")
--- evaluate.py
+++ evaluate.py
@@ -9,7 +9,141 @@ from setup import *
 from sklearn.metrics import f1_score, precision_recall_curve, confusion_matrix
 import matplotlib.pyplot as plt
 
-eval_df = pd.DataFrame(columns=[DATASET_NAME, METHOD, PAIRS, TP, TN, FP, FN, ACCURACY, PRECISION, RECALL, THRESHOLD, F1])
+def eval(df, bin_id, threshold, method, eval_df):
+    actual = pd.Series(df[PARAPHRASE], name='Actual')
+    predicted = pd.Series(df[bin_id], name='Predicted')
+    df_cross = pd.crosstab(actual, predicted)
+    #print(df_cross)
+    f1 = f1_score(actual, predicted)
+
+    # if there were no predicted non-paraphrases insert 0
+    try:
+        tp = df_cross[1][1]
+    except Exception as e:
+        tp = 0
+        pass
+    try:
+        tn = df_cross[0][0]
+    except Exception as e:
+        tn = 0
+        pass
+    try:
+        fp = df_cross[1][0]
+    except Exception as e:
+        fp = 0
+        pass
+    try:
+        fn = df_cross[0][1]
+    except Exception as e:
+        fn = 0
+        pass
+
+    eval_df.loc[len(eval_df.index)] = [
+        file.split("_")[0], 
+        method, 
+        str(len(df)),   # amount of sentence pairs in dataset
+        tp, 
+        tn, 
+        fp, 
+        fn, 
+        (tn+tp) / len(df),   # accuracy
+        tp / (tp+fp),   # precision
+        tp / (tp+fn),   # recall
+        tn / (tn+fp),    # specificity (true negative rate)
+        fp / (tn+fp),   # false positive rate
+        threshold, 
+        f1
+        ]
+    return eval_df
+
+def find_optimal_thresholds(df):
+    print("Finding the optimal thresholds for each method...")
+    SEM_BERT_THRESHOLD = 0.1
+    FUZZY_THRESHOLD = 0.1
+    SEM_T5_THRESHOLD = 0.1
+    NGRAM_THRESHOLD = 0.1
+    TFIDF_THRESHOLD = 0.1
+    sem_bert_best_f1 = 0.0
+    fuz_best_f1 = 0.0
+    sem_t5_best_f1 = 0.0
+    ngram_best_f1 = 0.0
+    tfidf_best_f1 = 0.0
+
+    for threshold in tqdm(np.round(np.linspace(0,1,100,endpoint=False),2)):
+        sem_bert_bin = []
+        fuzzy_bin = []
+        sem_t5_bin = []
+        ngram_bin = []
+        tfidf_bin = []
+        for i, row in df.iterrows():
+            # sem_bert
+            if row[SEM_BERT] >= threshold:
+                sem_bert_bin.append(True)
+            else:
+                sem_bert_bin.append(False)
+            # sem_t5
+            if row[SEM_T5] >= threshold:
+                sem_t5_bin.append(True)
+            else:
+                sem_t5_bin.append(False)
+            # fuzzy
+            if row[FUZZY] >= threshold:
+                fuzzy_bin.append(True)
+            else:
+                fuzzy_bin.append(False)
+            # ngram
+            if row[NGRAM] >= threshold:
+                ngram_bin.append(True)
+            else:
+                ngram_bin.append(False)
+            # tfidf cosine
+            if row[TFIDF_COSINE] >= threshold:
+                tfidf_bin.append(True)
+            else:
+                tfidf_bin.append(False)
+
+        df[SEM_BERT_BIN] = sem_bert_bin
+        df[SEM_T5_BIN] = sem_t5_bin
+        df[FUZZY_BIN] = fuzzy_bin
+        df[NGRAM_BIN] = ngram_bin
+        df[TFIDF_COSINE_BIN] = tfidf_bin
+
+        actual = pd.Series(df[PARAPHRASE], name='Actual')
+
+        predicted = pd.Series(df[SEM_BERT_BIN], name='Predicted')
+        f1 = f1_score(actual, predicted)
+        if f1 > sem_bert_best_f1:
+            sem_bert_best_f1 = f1
+            SEM_BERT_THRESHOLD = threshold 
+            
+        predicted = pd.Series(df[SEM_T5_BIN], name='Predicted')
+        f1 = f1_score(actual, predicted)
+        if f1 > sem_t5_best_f1:
+            sem_t5_best_f1 = f1
+            SEM_T5_THRESHOLD = threshold 
+
+        predicted = pd.Series(df[FUZZY_BIN], name='Predicted')
+        f1 = f1_score(actual, predicted)
+        if f1 > fuz_best_f1:
+            fuz_best_f1 = f1
+            FUZZY_THRESHOLD = threshold 
+        
+        predicted = pd.Series(df[NGRAM_BIN], name='Predicted')
+        f1 = f1_score(actual, predicted)
+        if f1 > ngram_best_f1:
+            ngram_best_f1 = f1
+            NGRAM_THRESHOLD = threshold 
+        
+        predicted = pd.Series(df[TFIDF_COSINE_BIN], name='Predicted')
+        f1 = f1_score(actual, predicted)
+        if f1 > tfidf_best_f1:
+            tfidf_best_f1 = f1
+            TFIDF_THRESHOLD = threshold 
+    
+    return TFIDF_THRESHOLD, SEM_BERT_THRESHOLD, SEM_T5_THRESHOLD, FUZZY_THRESHOLD, NGRAM_THRESHOLD 
+    
+
+eval_df = pd.DataFrame(columns=[DATASET_NAME, METHOD, PAIRS, TP, TN, FP, FN, ACCURACY, PRECISION, RECALL, SPECIFICITY, TRUE_NEG_RATE, THRESHOLD, F1])
 
 eval_string = ""
 eval_string += "-------------------------" + "\n"
@@ -27,135 +161,104 @@ threshold_df = threshold_df[(threshold_df[TEXT1] != "") & (threshold_df[TEXT2] !
 
 for file in os.listdir(os.path.join(OUT_DIR, DETECTION_FOLDER)):
     print(f"---> Evaluating {file}...")
+    if "MPCBert" not in file:
+        continue
     df = pd.read_json(os.path.join(OUT_DIR, DETECTION_FOLDER, file), orient = "index")
 
     # Find the optimal thresholds:
-    SEMANTIC_THRESHOLD = 0.1
-    FUZZY_THRESHOLD = 0.1
-    sem_best_f1 = 0.0
-    fuz_best_f1 = 0.0
-
-    print("Finding the optimal thresholds for this dataset...")
-    for threshold in tqdm(np.round(np.linspace(0,1,100,endpoint=False),2)):
-        semantic_bin = []
-        fuzzy_bin = []
-        for i, row in df.iterrows():
-            # semantic
-            if row[SEMANTIC] >= threshold:
-                semantic_bin.append(True)
-            else:
-                semantic_bin.append(False)
-            # fuzzy
-            if row[FUZZY] >= threshold:
-                fuzzy_bin.append(True)
-            else:
-                fuzzy_bin.append(False)
-
-        df[SEMANTIC_BIN] = semantic_bin
-        df[FUZZY_BIN] = fuzzy_bin
-
-        actual = pd.Series(df["is_paraphrase"], name='Actual')
-        predicted = pd.Series(df[SEMANTIC_BIN], name='Predicted')
-        f1 = f1_score(actual, predicted)
-        if f1 > sem_best_f1:
-            sem_best_f1 = f1
-            SEMANTIC_THRESHOLD = threshold 
-
-        actual = pd.Series(df["is_paraphrase"], name='Actual')
-        predicted = pd.Series(df[FUZZY_BIN], name='Predicted')
-        f1 = f1_score(actual, predicted)
-        if f1 > sem_best_f1:
-            fuz_best_f1 = f1
-            FUZZY_THRESHOLD = threshold 
+    if len(df[df[PARAPHRASE] == False]) > 10:
+        TFIDF_THRESHOLD, SEM_BERT_THRESHOLD, SEM_T5_THRESHOLD, FUZZY_THRESHOLD, NGRAM_THRESHOLD = find_optimal_thresholds(df)
+    else:
+        TFIDF_THRESHOLD, SEM_BERT_THRESHOLD, SEM_T5_THRESHOLD, FUZZY_THRESHOLD, NGRAM_THRESHOLD = 0.8, 0.8, 0.8, 0.8, 0.8
 
     print("Continue with thresholds: ")
-    print("Semantic: " + str(SEMANTIC_THRESHOLD))
-    print("Fuzzy: " + str(FUZZY_THRESHOLD))
+    print("sem_bert: " + str(SEM_BERT_THRESHOLD))
+    print("sem_t5: " + str(SEM_T5_THRESHOLD))
+    print("fuzzy: " + str(FUZZY_THRESHOLD))
+    print("ngram: " + str(NGRAM_THRESHOLD))
 
-    semantic_bin = []
+    # evaluate results with the optimal thresholds:
+    sem_bert_bin = []
+    sem_t5_bin = []
     fuzzy_bin = []
-    print("Checking defined thresholds for every pair...")
+    ngram_bin = []
+    tfidf_bin = []
+    print("Checking determined thresholds for every pair...")
     for i, row in df.iterrows():
-        # semantic
-        if row[SEMANTIC] >= SEMANTIC_THRESHOLD:
-            semantic_bin.append(True)
+        # sem_bert
+        if row[SEM_BERT] >= SEM_BERT_THRESHOLD:
+            sem_bert_bin.append(True)
         else:
-            semantic_bin.append(False)
+            sem_bert_bin.append(False)
+        # sem_t5
+        if row[SEM_T5] >= SEM_T5_THRESHOLD:
+            sem_t5_bin.append(True)
+        else:
+            sem_t5_bin.append(False)
         # fuzzy
         if row[FUZZY] >= FUZZY_THRESHOLD:
             fuzzy_bin.append(True)
         else:
             fuzzy_bin.append(False)
+        # ngram
+        if row[NGRAM] >= NGRAM_THRESHOLD:
+            ngram_bin.append(True)
+        else:
+            ngram_bin.append(False)
+        # tfidf cosine
+        if row[TFIDF_COSINE] >= TFIDF_THRESHOLD:
+            tfidf_bin.append(True)
+        else:
+            tfidf_bin.append(False)
 
-    df[SEMANTIC_BIN] = semantic_bin
+    df[SEM_BERT_BIN] = sem_bert_bin
+    df[SEM_T5_BIN] = sem_t5_bin
     df[FUZZY_BIN] = fuzzy_bin
+    df[NGRAM_BIN] = ngram_bin
+    df[TFIDF_COSINE] = tfidf_bin
 
     print("Calculating precision-recall curves...")
-    precision_semantic, recall_semantic, thresholds_semantic = precision_recall_curve(df["is_paraphrase"], df[SEMANTIC])
+    precision_sem_bert, recall_sem_bert, thresholds_sem_bert = precision_recall_curve(df["is_paraphrase"], df[SEM_BERT])
+    precision_sem_t5, recall_sem_t5, thresholds_sem_t5 = precision_recall_curve(df["is_paraphrase"], df[SEM_T5])
     precision_fuzzy, recall_fuzzy, thresholds_fuzzy = precision_recall_curve(df["is_paraphrase"], df[FUZZY])
+    precision_tfidf, recall_tfidf, thresholds_tfidf = precision_recall_curve(df["is_paraphrase"], df[TFIDF_COSINE])
 
     print("Plotting curves...")
     fig, ax = plt.subplots()
-    ax.plot(recall_semantic, precision_semantic, color='blue')
-    ax.set_title('Precision-Recall Curve (Semantic)')
+    ax.plot(recall_sem_bert, precision_sem_bert, color='blue')
+    ax.set_title(file.split("_")[0]+' Precision-Recall Curve (Bert)')
     ax.set_xlabel('Recall')
     ax.set_ylabel('Precision')
-    plt.savefig(os.path.join(OUT_DIR, EVALUATION_FOLDER, file.split("_")[0]+"_pr_semantic"))
+    plt.savefig(os.path.join(OUT_DIR, EVALUATION_FOLDER, file.split("_")[0]+"_pr_sem_bert"))
+
+    fig, ax = plt.subplots()
+    ax.plot(recall_sem_t5, precision_sem_t5, color='blue')
+    ax.set_title(file.split("_")[0]+' Precision-Recall Curve (T5)')
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    plt.savefig(os.path.join(OUT_DIR, EVALUATION_FOLDER, file.split("_")[0]+"_pr_sem_t5"))
 
     fig, ax = plt.subplots()
     ax.plot(recall_fuzzy, precision_fuzzy, color='blue')
-    ax.set_title('Precision-Recall Curve (Fuzzy)')
+    ax.set_title(file.split("_")[0]+' Precision-Recall Curve (Fuzzy)')
     ax.set_xlabel('Recall')
     ax.set_ylabel('Precision')
     plt.savefig(os.path.join(OUT_DIR, EVALUATION_FOLDER, file.split("_")[0]+"_pr_fuzzy"))
 
+    fig, ax = plt.subplots()
+    ax.plot(recall_tfidf, precision_tfidf, color='blue')
+    ax.set_title(file.split("_")[0]+' Precision-Recall Curve (TF-IDF Cosine)')
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    plt.savefig(os.path.join(OUT_DIR, EVALUATION_FOLDER, file.split("_")[0]+"_pr_tfidf_cos"))
+
     # Confusion Matrix and f1 score for the defined threshold
     print("Constructing confusion matrix & calculating f1-score...")
-    
-    actual = pd.Series(df["is_paraphrase"], name='Actual')
-    predicted = pd.Series(df[SEMANTIC_BIN], name='Predicted')
-    df_cross = pd.crosstab(actual, predicted)
-    f1 = f1_score(actual, predicted)
 
-    tp = df_cross[1][1]
-    tn = df_cross[0][0]
-    fp = df_cross[1][0]
-    fn = df_cross[0][1]
-    eval_df.loc[len(eval_df.index)] = [
-        file.split("_")[0], 
-        "semantic", 
-        str(len(df)), 
-        tp, 
-        tn, 
-        fp, 
-        fn, 
-        (tn+tp) / len(df), 
-        tp / (tp+fp), 
-        tp / (tp+fn), 
-        SEMANTIC_THRESHOLD, 
-        f1
-        ]
-
-    actual = pd.Series(df["is_paraphrase"], name='Actual')
-    predicted = pd.Series(df[FUZZY_BIN], name='Predicted')
-    df_cross = pd.crosstab(actual, predicted)
-    f1 = f1_score(actual, predicted)
-
-    eval_df.loc[len(eval_df.index)] = [
-        file.split("_")[0], 
-        "fuzzy", 
-        str(len(df)), 
-        tp, 
-        tn, 
-        fp, 
-        fn, 
-        (tn+tp) / len(df), 
-        tp / (tp+fp), 
-        tp / (tp+fn), 
-        FUZZY_THRESHOLD, 
-        f1
-        ]
-
+    eval_df = eval(df, SEM_BERT_BIN, SEM_BERT_THRESHOLD, SEM_BERT, eval_df)
+    eval_df = eval(df, SEM_T5_BIN, SEM_T5_THRESHOLD, SEM_T5, eval_df)
+    eval_df = eval(df, FUZZY_BIN, FUZZY_THRESHOLD, FUZZY, eval_df)
+    eval_df = eval(df, TFIDF_COSINE_BIN, TFIDF_THRESHOLD, TFIDF_COSINE, eval_df)
 
 eval_df.to_json(os.path.join(OUT_DIR, EVALUATION_FOLDER, EVALUATION_RESULTS_FILENAME), orient = "index", index = True, indent = 4)
 eval_df.to_csv(os.path.join(OUT_DIR, EVALUATION_FOLDER, EVALUATION_RESULTS_FILENAME.replace(".json", ".csv")), index = True)
